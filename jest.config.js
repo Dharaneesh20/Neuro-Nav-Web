decf210ab101f99e6bc2c358ee2d4bb63884cce5
Dharaneesh20 <dharaneeshrs777@gmail.com>
2026-02-21 15:40:09 +0530

    Config: Add project roadmap, Jest, ESLint, Prettier configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// Jest configuration for testing
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'server/**/*.js',
+    '!server/index.js',
+    '!server/node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  setupFilesAfterEnv: ['./server/tests/setup.js'],
+  testTimeout: 10000,
+};
